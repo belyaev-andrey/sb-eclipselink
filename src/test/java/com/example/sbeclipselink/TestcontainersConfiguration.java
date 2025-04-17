6e063e9566aea7c4225a447510d063738ea7c1e9
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-04-17 16:35:50 +0200

    Broken commit, fixing

--- src/test/java/com/example/sbeclipselink/TestcontainersConfiguration.java
+++ src/test/java/com/example/sbeclipselink/TestcontainersConfiguration.java
@@ -3,11 +3,15 @@ package com.example.sbeclipselink;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
